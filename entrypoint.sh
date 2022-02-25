74e20629657d72d422e1642f1c043970d41bf0ee
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2022-02-26 06:46:13 +0900

    Removed unused functions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-ANSIBLE_DIRECTORY="/opt/ansible"
-PYTHON_VIRTUALENV_DIRECTORY="venv"
+# A directory that an Ansible playbooks are located in.
+ANSIBLE_DIRECTORY_PATH="${ANSIBLE_DIRECTORY:-/opt/ansible}"
 
-PYTHON_VIRTUALENV_PATH="${ANSIBLE_DIRECTORY}/${PYTHON_VIRTUALENV_DIRECTORY}"
+# A directory path that python venv will be installed.
+# It will be created under ANSIBLE_DIRECTORY if you specify relative path.
+PYTHON_VIRTUALENV_DIRECTORY_PATH="${PYTHON_VIRTUALENV_DIRECTORY_PATH:-venv}"
 
-REQUIREMENTS_TXT_PATH="${ANSIBLE_DIRECTORY}/requirements.txt"
-REQUIREMENTS_TXT_HASH_PATH="${PYTHON_VIRTUALENV_PATH}/.requirements.txt.hash"
-
-REQUIREMENTS_YML_PATH="${ANSIBLE_DIRECTORY}/requirements.yml"
-REQUIREMENTS_YML_HASH_PATH="${PYTHON_VIRTUALENV_PATH}/.requirements.yml.hash"
+# A path of requirements.txt which information of requirement packages of python are in.
+REQUIREMENTS_TXT_PATH="${REQUIREMENTS_TXT_PATH:-requirements.txt}"
+# A path of requirements.yml which information of requirement packages of ansible galaxy are in.
+REQUIREMENTS_YML_PATH="${REQUIREMENTS_YML_PATH:-requirements.yml}"
 
 log_err() {
     echo "ERROR: $1" >&2
@@ -44,35 +45,35 @@ main() {
     }
 
     #${ANSIBLE_DIRECTORY}/${PYTHON_VIRTUALENV_DIRECTORY}/bin/ansible-playbook --user ubuntu -i "$inventry_file" -l "$target" site.yml
-    ${ANSIBLE_DIRECTORY}/${PYTHON_VIRTUALENV_DIRECTORY}/bin/ansible-playbook $@
+    ${ANSIBLE_DIRECTORY_PATH}/${PYTHON_VIRTUALENV_DIRECTORY_PATH}/bin/ansible-playbook $@
 }
 
-create_ansible_environment() {
-    # .gitignore in venv directory will also be usedas a flag file
-    # whether Ansible environment has already been prepared or not.
-    local prepared_flag_file="${ANSIBLE_DIRECTORY}/${PYTHON_VIRTUALENV_DIRECTORY}/.gitignore"
-
-    # Create symbolic link to cache packages of ansible-galaxy
-    ln -s "${ANSIBLE_DIRECTORY}/.ansible" ~/.ansible
+activate_python_virtual_env() {
     # venv directory has already been prepared?
     venv_has_already_prepared || {
-        python3 -m "$PYTHON_VIRTUALENV_DIRECTORY" "${PYTHON_VIRTUALENV_DIRECTORY}/" || {
+        python3 -m "$PYTHON_VIRTUALENV_DIRECTORY_PATH" "${PYTHON_VIRTUALENV_DIRECTORY_PATH}/" || {
             log_err "Failed to install python virtual env in $ANSIBLE_DIRECTORY"
             return 1
         }
-        ${ANSIBLE_DIRECTORY}/${PYTHON_VIRTUALENV_DIRECTORY}/bin/pip install --upgrade pip
+        ${ANSIBLE_DIRECTORY}/${PYTHON_VIRTUALENV_DIRECTORY_PATH}/bin/pip install --upgrade pip
     }
 
-    pip_packages_has_already_up_to_date || {
-        ${ANSIBLE_DIRECTORY}/${PYTHON_VIRTUALENV_DIRECTORY}/bin/pip install -r requirements.txt
+    source ${PYTHON_VIRTUALENV_DIRECTORY_PATH}/bin/activate
+}
+
+create_ansible_environment() {
+    # Create symbolic link to cache packages of ansible-galaxy
+    ln -s "${ANSIBLE_DIRECTORY}/.ansible" ~/.ansible
+
+    pip install -r "${REQUIREMENTS_TXT_PATH}" || {
+        log_err "Failed to install requirements with a command \"pip install -r ${REQUIREMENTS_TXT_PATH}\"."
+        return 1
     }
 
-    ansible_galaxy_packages_has_already_up_to_date || {
-        ${ANSIBLE_DIRECTORY}/${PYTHON_VIRTUALENV_DIRECTORY}/bin/ansible-galaxy install -r requirements.yml
+    ansible-galaxy install -r "${REQUIREMENTS_YML_PATH}" || {
+        log_err "Failed to install requirements with a command \"ansible-galaxy install -r \"${REQUIREMENTS_YML_PATH}\"."
+        return 1
     }
-
-    # Create .gitignore under venv directory not to commit.
-    [ ! -f "$prepared_flag_file" ] && echo "/*" > "$prepared_flag_file"
 
     return 0
 }
@@ -107,29 +108,6 @@ EOF
 
 venv_has_already_prepared() {
     [ ! -d "${PYTHON_VIRTUALENV_DIRECTORY}" ] && return 1
-    return 0
-}
-
-pip_packages_has_already_up_to_date() {
-    _packages_has_already_up_to_date "$REQUIREMENTS_TXT_PATH" "$REQUIREMENTS_TXT_HASH_PATH"
-}
-ansible_galaxy_packages_has_already_up_to_date() {
-    _packages_has_already_up_to_date "$REQUIREMENTS_YML_PATH" "$REQUIREMENTS_YML_HASH_PATH"
-}
-
-_packages_has_already_up_to_date() {
-    local package_file_path="$1"
-    local hash_file_path="$2"
-
-    [ ! -f "${package_file_path}" ] && return 0
-    [ ! -f "${hash_file_path}" ] && return 1
-
-    local old_hash current_hash
-    read old_hash < "${hash_file_path}"
-    read current_hash _ <<< "$(sha256sum ${package_file_path})"
-
-    [ ! "$old_hash" == "$current_hash" ] && return 1
-
     return 0
 }
 
